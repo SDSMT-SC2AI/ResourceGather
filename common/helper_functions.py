7f7e25fc24bec87989f8f4d2ae017b791fe8d0f5
macattackftw <macattackftw@users.noreply.github.com>
2018-03-09 18:31:28 -0700

    Wrote function to extract the number of larva at each of our expected bases.

---
+++ common/helper_functions.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+
+class Alliance(Enum):
+    """Enumerator for alliance"""
+    Self = 1
+    Ally = 2
+    Neutral = 3
+    Enemy = 4
+
+
+
+def GetUnits(unit_type, obs, alliance=Alliance.Self):
+    """Gets all units of unit_type(s), unit_type MUST be a list"""
+    ret_val = []
+    if (isinstance(unit_type, int)):
+        for item in obs[0].observation.raw_data.units:
+            if (item.alliance == alliance.value and item.unit_type == unit_type):
+                ret_val.append(item)
+    else:
+        for item in obs[0].observation.raw_data.units:
+            if (item.alliance == alliance.value and item.unit_type in unit_type):
+                ret_val.append(item)
+    return ret_val
+
+
+def DistSquared(a, b):
+    """Distance squared algorithm, requires Point{x: y: z:}"""
+    return (a.x - b.x )**2 + (a.y - b.y)**2 + (a.z - b.z)**2
+
+
+def InDistSqRange(a, b, range):
+    """Checks to see if a is in range of b"""
+    return DistSquared(a, b) < range
+
+def InRadius(a, b, radius):
+    """Checks to see if a is in range of b"""
+    return sqrt(DistSquared(a, b)) < radius
--- observer.py
+++ observer.py
@@ -11,7 +11,7 @@ def process_observation(env_obs, flags=None):
     food_available = np.array([env_obs.raw_obs.player_common.food_cap - env_obs.raw_obs.player_common.food_used])
     bases = GetUnits(86, env_obs)
     number_of_bases = np.array([len(bases) / flags.max_bases])
-    larva_by_base = np.array(get_larva_by_base(env_obs, bases, flags))
+    larva_by_base = np.asarray(get_larva_by_base(env_obs, bases, flags))
     obs = np.concatenate([available_actions, minerals, food_available, number_of_bases, larva_by_base])
     episode_end = (env_obs.observation.step_type == environment.StepType.LAST)
     return reward, obs, episode_end
@@ -19,5 +19,16 @@ def process_observation(env_obs, flags=None):
 
 
 def get_larva_by_base(env_obs, bases, flags=None):
-    larva = [0]*flags.max_bases
-    return larva
+    """Returns a list of lists where base_larva[i] are base[i]'s larva"""
+    base_larva = []
+    for base in bases:
+        base_larva_list = 0
+        for larva in all_larva:
+            if (InDistSqRange(base.pos, larva.pos, 12)): # Larva are < 9 dist away, gave a buffer
+                base_larva_list += 1
+        if (base_larva_list != 0):
+            base_larva.append(base_larva_list)
+    for i in range(flags.max_bases - len(base_larva)):
+        base_larva.append([])
+    
+    return base_larva
