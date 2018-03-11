e8516efac952d105d12c5617c4363403b6f6a5b5
macattackftw <macattackftw@users.noreply.github.com>
2018-03-11 14:07:14 -0600

    Reduced necessary code. Created one file to run all tests but each file can still be ran individually.

--- tests/test_Distances.py
+++ tests/test_Distances.py
@@ -2,16 +2,12 @@ from enum import Enum
 import sys
 sys.path.insert(0, '../common')
 import helper_functions as hlp
-from absl import flags
-from absl.flags import FLAGS
 from pysc2.env import sc2_env
+from absl.flags import FLAGS
 
-def main():
-    map_name = FLAGS.map_name
-    environ = sc2_env.SC2Env(map_name=map_name, agent_race="Z")
+def TestDistances():
+    environ = sc2_env.SC2Env(map_name="test_Distances", agent_race="Z")
     _obs = environ._parallel.run(c.observe for c in environ._controllers)
-
-
 
     marine = hlp.GetUnits([48],_obs, hlp.Alliance.Self)     # 0,0
     marauder = hlp.GetUnits([51],_obs, hlp.Alliance.Self)   # 4,3
@@ -30,11 +26,13 @@ def main():
         print("\nFailed InRadius\n")
         exit()
 
-    print("Distances() passed all tests")
+    print("Distances() passed all tests") 
+
+def main():
+    TestDistances()
     
 
 if __name__ == '__main__':
-    flags.DEFINE_string("map_name", "test_Distances", "Name of the map/Unit Test")
     FLAGS(sys.argv)
     main()
 
--- tests/test_GetUnits.py
+++ tests/test_GetUnits.py
@@ -2,19 +2,20 @@ from enum import Enum
 import sys
 sys.path.insert(0, '../common')
 import helper_functions as hlp
-from absl import flags
-from absl.flags import FLAGS
 from pysc2.env import sc2_env
+from absl.flags import FLAGS
 
-def main():
-    map_name = FLAGS.map_name
-    environ = sc2_env.SC2Env(map_name=map_name, agent_race="Z")
+def TestGetUnits():
+    environ = sc2_env.SC2Env(map_name="test_GetUnits", agent_race="Z")
     _obs = environ._parallel.run(c.observe for c in environ._controllers)
 
-
-
+    # Single type of friendly unit
     marines = hlp.GetUnits([48],_obs, hlp.Alliance.Self)
+
+    # Single type of enemy unit
     lings = hlp.GetUnits([105], _obs, hlp.Alliance.Enemy)
+
+    # Multi-type of friendly units
     player1 = hlp.GetUnits([48, 51],_obs, hlp.Alliance.Self)
 
 
@@ -32,8 +33,10 @@ def main():
 
     print("GetUnits() passed all tests")
 
+def main():
+    TestGetUnits()
+
 if __name__ == '__main__':
-    flags.DEFINE_string("map_name", "test_GetUnits", "Name of the map/Unit Test")
     FLAGS(sys.argv)
     main()
 
